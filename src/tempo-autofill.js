79c3bd86137dd2c4cba8074721f8a4c5f4491414
Victor Marquez <victmo@gmail.com>
2017-05-31 14:20:57 -0400

    Update tempo bookmarklet (interval)

--- src/tempo-autofill.js
+++ src/tempo-autofill.js
@@ -1,7 +1,15 @@
-// javascript:(()=%3E{%24%24=(s)=%3Edocument.querySelectorAll(s);%24%24(%27%23c55__CapitalProjectName_%2C%23popup_CapitalProjectName_%27)[0].value=%27APE-BI-Unified%20Template%27;%24%24(%27%23c55__KTLOAdmin%5C%5C(ifapplicable%5C%5C)_%2C%23popup_KTLOAdmin%5C%5C(ifapplicable%5C%5C)_%27)[0].value=%27Project%27;%24%24(%27%23c55__CapitalActivity_%2C%23popup_CapitalActivity_%27)[0].value=%27Design%20and%20Development%20-%20CAPITAL%20%27;})();
+// javascript:(()=%3E{window.victmo%20=%20window.victmo%20%7C%7C%20{};const%20setValue%20=%20(s%2Cv)%20=%3E%20{const%20els%20=%20[...document.querySelectorAll(s)];els.filter(el%20=%3E%20typeof%20el.value%20===%20%27string%27).forEach(el%20=%3E%20el.value%20=%20v);};const%20autofill%20=%20()%20=%3E%20{setValue(%27select[id%24=_CapitalProjectName_]%27%2C%20%27APE-BI-Unified%20Template%27);setValue(%27select[id%24=_KTLOAdmin%5C%5C(ifapplicable%5C%5C)_]%27%2C%20%27Project%27);setValue(%27select[id%24=_CapitalActivity_]%27%2C%20%27Design%20and%20Development%20-%20CAPITAL%20%27);};if(window.victmo.tempoIntervalId)%20clearInterval(window.victmo.tempoIntervalId);window.victmo.tempoIntervalId%20=%20setInterval(autofill%2C%203000);})();
 (()=>{
-	$$=(s)=>document.querySelectorAll(s);
-	$$('#c55__CapitalProjectName_,#popup_CapitalProjectName_')[0].value='APE-BI-Unified Template';
-	$$('#c55__KTLOAdmin\\(ifapplicable\\)_,#popup_KTLOAdmin\\(ifapplicable\\)_')[0].value='Project';
-	$$('#c55__CapitalActivity_,#popup_CapitalActivity_')[0].value='Design and Development - CAPITAL ';
+	window.victmo = window.victmo || {};
+	const setValue = (s,v) => {
+		const els = [...document.querySelectorAll(s)];
+		els.filter(el => typeof el.value === 'string').forEach(el => el.value = v);
+	};
+	const autofill = () => {
+		setValue('select[id$=_CapitalProjectName_]', 'APE-BI-Unified Template');
+		setValue('select[id$=_KTLOAdmin\\(ifapplicable\\)_]', 'Project');
+		setValue('select[id$=_CapitalActivity_]', 'Design and Development - CAPITAL ');
+	};
+	if(window.victmo.tempoIntervalId) clearInterval(window.victmo.tempoIntervalId);
+	window.victmo.tempoIntervalId = setInterval(autofill, 3000);
 })();
