9a909adca46161f8b519bdbab6cb6127a9136fe9
Victor Marquez <victmo@gmail.com>
2017-05-31 12:31:02 -0400

    Add Tempo autofill bookmarklet

---
+++ src/tempo-autofill.js
@@ -0,0 +1,7 @@
+// javascript:(()=%3E{%24%24=(s)=%3Edocument.querySelectorAll(s);%24%24(%27%23c55__CapitalProjectName_%2C%23popup_CapitalProjectName_%27)[0].value=%27APE-BI-Unified%20Template%27;%24%24(%27%23c55__KTLOAdmin%5C%5C(ifapplicable%5C%5C)_%2C%23popup_KTLOAdmin%5C%5C(ifapplicable%5C%5C)_%27)[0].value=%27Project%27;%24%24(%27%23c55__CapitalActivity_%2C%23popup_CapitalActivity_%27)[0].value=%27Design%20and%20Development%20-%20CAPITAL%20%27;})();
+(()=>{
+	$$=(s)=>document.querySelectorAll(s);
+	$$('#c55__CapitalProjectName_,#popup_CapitalProjectName_')[0].value='APE-BI-Unified Template';
+	$$('#c55__KTLOAdmin\\(ifapplicable\\)_,#popup_KTLOAdmin\\(ifapplicable\\)_')[0].value='Project';
+	$$('#c55__CapitalActivity_,#popup_CapitalActivity_')[0].value='Design and Development - CAPITAL ';
+})();
